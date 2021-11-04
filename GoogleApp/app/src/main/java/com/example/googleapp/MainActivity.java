8cadf46b8be6f23c2982e2f9b813050bcd2ef584
RupamDhar <rupamdhar1000@gmail.com>
2021-11-04 19:33:39 +0530

    Default Commit

---
+++ GoogleApp/app/src/main/java/com/example/googleapp/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.googleapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity
+{
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webView);
+        String url = "https://www.google.com";
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(url);
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        if(webView.canGoBack()) webView.goBack();
+        else super.onBackPressed();
+    }
+}
