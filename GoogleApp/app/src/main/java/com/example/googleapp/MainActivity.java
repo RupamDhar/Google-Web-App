936f1544405e9411d85eb746d845d4c7b091742e
RupamDhar <rupamdhar1000@gmail.com>
2021-11-04 19:43:33 +0530

    Second Commit

--- GoogleApp/app/src/main/java/com/example/googleapp/MainActivity.java
+++ GoogleApp/app/src/main/java/com/example/googleapp/MainActivity.java
@@ -15,6 +15,8 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getSupportActionBar().hide();
+
         webView = (WebView) findViewById(R.id.webView);
         String url = "https://www.google.com";
 
